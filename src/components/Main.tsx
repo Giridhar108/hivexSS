45fbc8aec08206835b23a99bc7856f13ddee3801
giridhar <pentagram777@mail.ru>
2021-10-14 17:30:45 +0500

    split code in main console

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,64 +1,15 @@
-import React, {useEffect} from 'react';
-import {useAppDispatch, useAppSelector} from '../app/hooks';
-import {ActionTypes} from '../constants/sagaActions';
-import {MAIN_QUERY, MAIN_RESPONSE} from '../constants/variables';
-import {isJsonString} from '../helpers/isJsonString';
-import {UseFormField} from '../hooks/useFormField';
-import {Label, MainConsole, SectionQuery, Spliter, Textarea, WrapperColumn} from '../style/components/MainConsole';
-import { spliterSvg } from '../svg/spliterSvg';
-import { Imain } from '../types/types';
+import React from 'react';
+import {MainConsole} from '../style/components/MainConsole';
+import {Imain} from '../types/types';
 import HistoryResponse from './HistoryResponse';
+import MainSectionQuery from './MainSectionQuery';
 
 function Main({send, format, setSend, setFormat}: Imain) {
-  const dispatch = useAppDispatch();
-  const {oneQuery} = useAppSelector((state) => state.query);
-
-  const [clear, setclear] = React.useState(false);
-  const [errorQuery, setErrorQuery] = React.useState(false);
-  const [response, setResponse] = React.useState('');
-  const textareaOne = UseFormField(clear, setclear);
-
-  useEffect(() => {
-
-    if (send && isJsonString(textareaOne.value)) {
-      dispatch({type: ActionTypes.QUERY, payload: JSON.parse(textareaOne.value)});
-      setSend(false);
-      setErrorQuery(false);
-    }
-
-    if (!isJsonString(textareaOne.value) && textareaOne.value.length > 0) {
-      setSend(false);
-      setErrorQuery(true);
-    }
-
-    if (format && isJsonString(textareaOne.value)) {
-      textareaOne.onChange(JSON.stringify(JSON.parse(textareaOne.value), null, 3));
-      setFormat(false);
-    }
-  }, [send, format]);
-
-  useEffect(() => {
-    setResponse(oneQuery.response !== '' ? JSON.stringify(JSON.parse(oneQuery.response), null, 3) : '');
-  }, [oneQuery]);
 
   return (
     <MainConsole>
-      <HistoryResponse/>
-      <SectionQuery>
-        <WrapperColumn>
-          <Label colorLabel={errorQuery} htmlFor="one">
-            {MAIN_QUERY}
-          </Label>
-          <Textarea colorLabel={errorQuery} name="one" id="" {...textareaOne}></Textarea>
-        </WrapperColumn>
-        <Spliter>
-          {spliterSvg}
-        </Spliter>
-        <WrapperColumn>
-          <Label colorLabel={oneQuery.status === 'reject'} htmlFor="two">{MAIN_RESPONSE}</Label>
-          <Textarea colorLabel={oneQuery.status === 'reject'} readOnly name="two" id="" defaultValue={response}></Textarea>
-        </WrapperColumn>
-      </SectionQuery>
+      <HistoryResponse />
+      <MainSectionQuery send={send} format={format} setSend={setSend} setFormat={setFormat} />
     </MainConsole>
   );
 }
